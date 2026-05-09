8f41a47e4cadf702383e2979552283fe77387a92
luiz-igreen <luizsilvaluizsilva052@gmail.com>
2026-05-08 23:31:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -532,4 +532,4 @@ app.post('/webhook/igreen', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));    
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
